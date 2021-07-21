42871e5d49192042edf9f62549d81ce590bcd2cc
AnkleBreaker <MagallonJanjan@gmail.com>
2021-07-21 17:07:16 +0800

    My angular

---
+++ src/app/second/second.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-second',
+  template: `<p>second works! <br><br> {{secondMessage ? secondMessage :'________'}}</p>`,
+  styles: ['p {color:red}']
+})
+export class SecondComponent  {
+
+@Input() secondMessage = "";
+ 
+}
